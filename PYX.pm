014d75667ddccd7b0e95689562ab8f9f20c6b2fd
Michal Špaček <tupinek@gmail.com>
2015-01-14 19:21:29 +0100

    First version.

---
+++ PYX.pm
@@ -0,0 +1,52 @@
+package Task::PYX;
+
+# Pragmas.
+use strict;
+use warnings;
+
+# Version.
+our $VERSION = 0.01;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Task::PYX - Install the PYX modules.
+
+=head1 SYNOPSIS
+
+ cpanm Task::PYX
+
+=head1 SEE ALSO
+
+L<PYX>,
+L<PYX::GraphViz>,
+L<PYX::Utils>.
+
+=head1 REPOSITORY
+
+L<https://github.com/tupinek/Task-PYX>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+ © 2015 Michal Špaček
+ Artistic License
+ BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
