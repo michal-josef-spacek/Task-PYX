0632a1ed50fdac9ee5ce04589e5b19fac2e57acb
Michal Špaček <tupinek@gmail.com>
2015-03-31 23:18:30 +0200

    Rewrite SEE ALSO section to NEILB format.

--- PYX.pm
+++ PYX.pm
@@ -25,16 +25,49 @@ Task::PYX - Install the PYX modules.
 
 =head1 SEE ALSO
 
-L<PYX>,
-L<PYX::GraphViz>,
-L<PYX::Hist>,
-L<PYX::Parser>,
-L<PYX::Stack>,
-L<PYX::Utils>,
-L<PYX::XMLNorm>,
-L<Tags::Output::PYX>,
-L<XML::MinWriter>,
-L<XML::PYX>.
+=over
+
+=item L<PYX>
+
+Common PYX handling module.
+
+=item L<PYX::GraphViz>
+
+GraphViz output for PYX handling.
+
+=item L<PYX::Hist>
+
+Processing PYX data or file and print histogram.
+
+=item L<PYX::Parser>
+
+PYX parser.
+
+=item L<PYX::Stack>
+
+Processing PYX data or file and process element stack.
+
+=item L<PYX::Utils>
+
+PYX common utilities for encoding and decoding data.
+
+=item L<PYX::XMLNorm>
+
+Processing PYX data or file and do XML normalization.
+
+=item L<Tags::Output::PYX>
+
+Tags output to PYX format.
+
+=item L<XML::MinWriter>
+
+Perl extension for writing XML in PYX format.
+
+=item L<XML::PYX>
+
+PYX parser based on XML::Parser.
+
+=back
 
 =head1 REPOSITORY
 
